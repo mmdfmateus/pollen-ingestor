5ad98ca6629ba714b4881ec3bbd554021c9549b3
Mateus Mourão <mateusm@take.net>
2021-04-05 01:16:09 -0300

    add swagger

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # pollen-ingestor
 Web API for ingesting an csv file with pollen information, as part of a distributed system.
+
+# Sample request
+curl --location --request POST 'http://localhost:9000/api/v1/upload-csv' --form 'file=@"/Path/To/Csv/File.csv"'
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import express, { json } from 'express'
+import swaggerUi from 'swagger-ui-express';
 
 import router from './routes.js';
+import swaggerDocument from '../swagger.json';
 
 const app = express();
 const port = 9000
 
 app.use(json());
+app.use('/api/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/v1', router);
 
 app.listen(port, function () {
